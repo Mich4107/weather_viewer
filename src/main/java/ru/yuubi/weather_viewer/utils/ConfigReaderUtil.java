151f48c7874c31356aedc1f6b4815684eac2f53f
Mich4107 <mich4107.ch@gmail.com>
2024-05-11 14:56:20 +0300

    refactor(util): edited logic of getApiKey method in ConfigReaderUtil

--- src/main/java/ru/yuubi/weather_viewer/utils/ConfigReaderUtil.java
+++ src/main/java/ru/yuubi/weather_viewer/utils/ConfigReaderUtil.java
@@ -1,14 +1,23 @@
 package ru.yuubi.weather_viewer.utils;
 
+import ru.yuubi.weather_viewer.service.AuthService;
+
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReaderUtil {
     private static Properties props = new Properties();
-    public static String getApiKey(){
-        try {
-            props.load(new FileInputStream("config.properties"));
+    public static String getApiKey() {
+
+        InputStream inputStream = ConfigReaderUtil.class.getResourceAsStream("/config.properties");
+        try (inputStream) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("Resource not found: config.properties");
+            }
+            props.load(inputStream);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
