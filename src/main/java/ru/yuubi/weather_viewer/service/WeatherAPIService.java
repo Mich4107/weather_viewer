56f1840bcada6af3ae8b02295c122a0aeb424990
Mich4107 <mich4107.ch@gmail.com>
2024-05-12 11:53:21 +0300

    refactor: logic of WeatherAPIService has been changed

--- src/main/java/ru/yuubi/weather_viewer/service/WeatherAPIService.java
+++ src/main/java/ru/yuubi/weather_viewer/service/WeatherAPIService.java
@@ -1,8 +1,11 @@
 package ru.yuubi.weather_viewer.service;
 
-import org.json.JSONObject;
-import org.json.JSONTokener;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.yuubi.weather_viewer.dto.WeatherDTO;
+import ru.yuubi.weather_viewer.dto.LocationDTO;
+import ru.yuubi.weather_viewer.exception.WeatherAPIException;
 import ru.yuubi.weather_viewer.utils.ConfigReaderUtil;
 
 import java.io.BufferedReader;
@@ -10,46 +13,66 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WeatherAPIService {
-    private static final String apiKey = ConfigReaderUtil.getApiKey();
-    private static final String url = "https://api.openweathermap.org/data/2.5/weather";
-    public WeatherDTO getWeatherByCityName(String name) {
-        String response;
-        try {
-            response = sendRequestToApi(name);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        JSONTokener jsonTokener = new JSONTokener(response);
-        JSONObject jsonObject = new JSONObject(jsonTokener);
-
-        return getWeatherDtoFromJson(jsonObject);
-    }
+    private static final String API_KEY = ConfigReaderUtil.getApiKey();
+    private static final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+    private static final String LOCATIONS_URL = "http://api.openweathermap.org/geo/1.0/direct";
 
     public WeatherDTO getWeatherByCoordinates(double lat, double lon){
-        String response;
+        String requestUrl = WEATHER_URL +"?lat="+lat+"&lon="+lon+"&appid="+ API_KEY +"&units=metric";
         try {
-            response = sendRequestToApi(lat, lon);
+            String jsonResponse = getResponse(requestUrl);
+            return getWeatherFromJson(jsonResponse);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        JSONTokener jsonTokener = new JSONTokener(response);
-        JSONObject jsonObject = new JSONObject(jsonTokener);
-
-        return getWeatherDtoFromJson(jsonObject);
     }
 
-    private String sendRequestToApi(String cityName) throws IOException {
-        String requestUrl = url+"?q="+cityName+"&appid="+apiKey;
-        return getResponse(requestUrl);
+    public List<LocationDTO> getLocationsByCityName(String name) {
+        String requestURL = LOCATIONS_URL+"?q="+name+"&limit="+10+"&appid="+API_KEY;
+        try {
+            String jsonResponse = getResponse(requestURL);
+            return getLocationsFromJson(jsonResponse);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public List<LocationDTO> getLocationsFromJson(String jsonResponse) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(jsonResponse);
+        List<LocationDTO> locations = new ArrayList<>();
+        for (int i = 0; i < rootNode.size(); i++) {
+            String name = rootNode.get(i).get("name").asText();
+            double lat = rootNode.get(i).get("lat").asDouble();
+            double lon = rootNode.get(i).get("lon").asDouble();
+            String countryCode = rootNode.get(i).get("country").asText();
+            String state = rootNode.get(i).get("state").asText();
+
+            LocationDTO locationDTO = new LocationDTO(name, lat, lon, countryCode, state);
+            locations.add(locationDTO);
+        }
+        return locations;
     }
 
-    private String sendRequestToApi(double latitude, double longitude) throws IOException {
-        String requestUrl = url+"?lat="+latitude+"&lon="+longitude+"&appid="+apiKey;
-        return getResponse(requestUrl);
+    public WeatherDTO getWeatherFromJson(String jsonResponse) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(jsonResponse);
+
+        double latitude = rootNode.get("coord").get("lat").asDouble();
+        double longitude = rootNode.get("coord").get("lon").asDouble();
+        String description = rootNode.get("weather").get(0).get("description").asText();
+        double temp = rootNode.get("main").get("temp").asDouble();
+        double tempFeelsLike = rootNode.get("main").get("feels_like").asDouble();
+        String locationName = rootNode.get("name").asText();
+        String countryCode = rootNode.get("sys").get("country").asText();
+        String iconId = rootNode.get("weather").get(0).get("icon").asText();
+
+        int roundedTemp = (int) Math.round(temp);
+        int roundedTempFeelsLike = (int) Math.round(tempFeelsLike);
+        return new WeatherDTO(longitude, latitude, description, roundedTemp, roundedTempFeelsLike, locationName, countryCode, iconId);
     }
 
     private String getResponse(String requestUrl) throws IOException {
@@ -72,21 +95,7 @@ public class WeatherAPIService {
 
             return response.toString();
         } else {
-            throw new RuntimeException("Can't reach request to the API");
+            throw new WeatherAPIException("Something went wrong in the request to the API");
         }
-    }
-
-    private WeatherDTO getWeatherDtoFromJson(JSONObject jsonObject){
-        double latitude = jsonObject.getJSONObject("coord").getDouble("lat");
-        double longitude = jsonObject.getJSONObject("coord").getDouble("lon");
-        String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
-        double temperature = jsonObject.getJSONObject("main").getDouble("temp");
-        double temperatureFeelsLike = jsonObject.getJSONObject("main").getDouble("feels_like");
-        String locationName = jsonObject.getString("name");
-
-        int tempInCelsius = (int) Math.round(temperature - 273.15);
-        int tempFeelsLikeInCelsius = (int) Math.round(temperatureFeelsLike - 273.15);
-
-        return new WeatherDTO(longitude, latitude, description, tempInCelsius, tempFeelsLikeInCelsius, locationName);
     }
 }
