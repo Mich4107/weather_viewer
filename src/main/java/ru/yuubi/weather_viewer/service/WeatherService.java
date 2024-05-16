0a239bd6c8bcab323e3372256a023b6f091fbe2a
Mich4107 <mich4107.ch@gmail.com>
2024-05-16 19:45:33 +0300

    feat(weatherService): check isUserAlreadyHasThisLocation(), methods for pagination

--- src/main/java/ru/yuubi/weather_viewer/service/WeatherService.java
+++ src/main/java/ru/yuubi/weather_viewer/service/WeatherService.java
@@ -5,11 +5,14 @@ import ru.yuubi.weather_viewer.dao.LocationDAO;
 import ru.yuubi.weather_viewer.dto.ResponseWeatherDTO;
 import ru.yuubi.weather_viewer.dto.WeatherDescriptionDTO;
 import ru.yuubi.weather_viewer.entity.Location;
-import ru.yuubi.weather_viewer.entity.SessionEntity;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.List;
 
 public class WeatherService {
+    private static final int ITEMS_PER_PAGE = 3;
     private LocationDAO locationDAO;
 
     public void saveLocation(Location location){
@@ -50,7 +53,65 @@ public class WeatherService {
         double lon = responseWeatherDTO.getLongitude();
 
         return new WeatherDescriptionDTO(locationInfo, weatherDescription, temp,
-                tempFeelsLike, pressure, humidity, windSpeed, iconUrl);
+                tempFeelsLike, pressure, humidity, windSpeed, iconUrl, lat, lon);
+    }
+
+    public boolean isUserAlreadyHasThisLocation(Location location) {
+        double lat = location.getLatitude();
+        double lon = location.getLongitude();
+
+        BigDecimal roundedLat = new BigDecimal(lat);
+        BigDecimal roundedLon = new BigDecimal(lon);
+
+        roundedLat = roundedLat.setScale(4, RoundingMode.HALF_UP);
+        roundedLon = roundedLon.setScale(4, RoundingMode.HALF_UP);
+
+        lat = Double.parseDouble(String.valueOf(roundedLat));
+        lon = Double.parseDouble(String.valueOf(roundedLon));
+
+        int userId = location.getUserId();
+
+        Location userLocation = locationDAO.getLocationByLatAndLon(lat, lon, userId);
+
+        return userLocation != null;
+    }
+
+    public int getPageNumber(String pageNumberFromReq) {
+        if(pageNumberFromReq == null) {
+            return  1;
+        } else {
+            return Integer.parseInt(pageNumberFromReq);
+        }
+    }
+
+    public int getTotalPages(int listSize) {
+        return (int) Math.ceil((double) listSize / ITEMS_PER_PAGE);
+    }
+
+    public int getStartIndex(int pageNumber) {
+        return (ITEMS_PER_PAGE*pageNumber)-ITEMS_PER_PAGE;
+    }
+
+    public int getEndIndex(int pageNumber, int listSize) {
+        return Math.min(ITEMS_PER_PAGE*pageNumber, listSize);
+    }
+
+
+
+    public List<WeatherDescriptionDTO> getDescriptions(List<Location> locations, WeatherApiService weatherApiService) {
+        List<WeatherDescriptionDTO> descriptionsOfUserLocations = new ArrayList<>();
+        for(Location location : locations) {
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+
+            ResponseWeatherDTO responseWeatherDTO = weatherApiService.getWeatherByCoordinates(lat, lon);
+
+            WeatherDescriptionDTO weatherDescription = createDescriptionFromResponseDto(responseWeatherDTO);
+            weatherDescription.setLocationId(location.getId());
+
+            descriptionsOfUserLocations.add(weatherDescription);
+        }
+        return descriptionsOfUserLocations;
     }
 
     public WeatherService() {
