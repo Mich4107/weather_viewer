074103687ea0953c781a05b3f29151af51b5fbaf
Mich4107 <mich4107.ch@gmail.com>
2024-05-23 16:12:04 +0300

    refactor: removed DeauthFilter and set this logic to DeauthServlet

--- src/main/java/ru/yuubi/weather_viewer/servlets/auth/DeauthenticationServlet.java
+++ src/main/java/ru/yuubi/weather_viewer/servlets/auth/DeauthenticationServlet.java
@@ -1,6 +1,7 @@
 package ru.yuubi.weather_viewer.servlets.auth;
 
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import ru.yuubi.weather_viewer.servlets.BaseServlet;
@@ -11,6 +12,22 @@ import java.io.IOException;
 public class DeauthenticationServlet extends BaseServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Cookie[] cookies = req.getCookies();
+        String searchCookieName = "sessionId";
+
+        if(cookies == null) {
+            resp.sendRedirect("/signin");
+            return;
+        }
+
+        for(Cookie cookie : cookies) {
+            if(cookie.getName().equals(searchCookieName)) {
+                String sessionGUIDValue = cookie.getValue();
+                cookie.setMaxAge(0);
+                resp.addCookie(cookie);
+                authService.removeSessionEntity(sessionGUIDValue);
+            }
+        }
         resp.sendRedirect("/signin");
     }
 
