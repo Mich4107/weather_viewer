6093f58d13087fb59342ddd227ba4edfb91d08e9
Mich4107 <mich4107.ch@gmail.com>
2024-05-23 16:02:54 +0300

    chore: now using lombok in dtos

--- src/main/java/ru/yuubi/weather_viewer/dto/ResponseWeatherDTO.java
+++ src/main/java/ru/yuubi/weather_viewer/dto/ResponseWeatherDTO.java
@@ -1,6 +1,11 @@
 package ru.yuubi.weather_viewer.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class ResponseWeatherDTO {
     private double longitude;
     private double latitude;
@@ -13,127 +18,5 @@ public class ResponseWeatherDTO {
     private String locationName;
     private String countryCode;
     private String iconId;
-
-    public ResponseWeatherDTO() {
-    }
-
-    public ResponseWeatherDTO(double longitude, double latitude, String description,
-                              int temp, int tempFeelsLike, int pressure, int humidity,
-                              double windSpeed, String locationName, String countryCode, String iconId) {
-        this.longitude = longitude;
-        this.latitude = latitude;
-        this.description = description;
-        this.temp = temp;
-        this.tempFeelsLike = tempFeelsLike;
-        this.pressure = pressure;
-        this.humidity = humidity;
-        this.windSpeed = windSpeed;
-        this.locationName = locationName;
-        this.countryCode = countryCode;
-        this.iconId = iconId;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public String getIconId() {
-        return iconId;
-    }
-
-    public void setIconId(String iconId) {
-        this.iconId = iconId;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
-    }
-
-    public int getTempFeelsLike() {
-        return tempFeelsLike;
-    }
-
-    public void setTempFeelsLike(int tempFeelsLike) {
-        this.tempFeelsLike = tempFeelsLike;
-    }
-
-    public String getLocationName() {
-        return locationName;
-    }
-
-    public void setLocationName(String locationName) {
-        this.locationName = locationName;
-    }
-
-    public int getPressure() {
-        return pressure;
-    }
-
-    public void setPressure(int pressure) {
-        this.pressure = pressure;
-    }
-
-    public int getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(int humidity) {
-        this.humidity = humidity;
-    }
-
-    public double getWindSpeed() {
-        return windSpeed;
-    }
-
-    public void setWindSpeed(double windSpeed) {
-        this.windSpeed = windSpeed;
-    }
-
-    @Override
-    public String toString() {
-        return "WeatherDTO{" +
-                "longitude=" + longitude +
-                ", latitude=" + latitude +
-                ", description='" + description + '\'' +
-                ", temp=" + temp +
-                ", tempFeelsLike=" + tempFeelsLike +
-                ", pressure=" + pressure +
-                ", humidity=" + humidity +
-                ", windSpeed=" + windSpeed +
-                ", locationName='" + locationName + '\'' +
-                ", countryCode='" + countryCode + '\'' +
-                '}';
-    }
 }
+
--- src/main/java/ru/yuubi/weather_viewer/dto/WeatherDescriptionDTO.java
+++ src/main/java/ru/yuubi/weather_viewer/dto/WeatherDescriptionDTO.java
@@ -1,5 +1,13 @@
 package ru.yuubi.weather_viewer.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class WeatherDescriptionDTO {
     private int locationId;
     private double lat;
@@ -12,10 +20,6 @@ public class WeatherDescriptionDTO {
     private String humidity;
     private String windSpeed;
     private String iconUrl;
-
-    public WeatherDescriptionDTO() {
-    }
-
     public WeatherDescriptionDTO(String locationInfo, String weatherDescription, String temp,
                                  String tempFeelsLike, String pressure, String humidity,
                                  String windSpeed, String iconUrl, double lat, double lon) {
@@ -28,94 +32,6 @@ public class WeatherDescriptionDTO {
         this.windSpeed = windSpeed;
         this.iconUrl = iconUrl;
         this.lat = lat;
-        this.lon = lon;
-    }
-
-    public int getLocationId() {
-        return locationId;
-    }
-
-    public void setLocationId(int locationId) {
-        this.locationId = locationId;
-    }
-
-    public String getLocationInfo() {
-        return locationInfo;
-    }
-
-    public void setLocationInfo(String locationInfo) {
-        this.locationInfo = locationInfo;
-    }
-
-    public String getWeatherDescription() {
-        return weatherDescription;
-    }
-
-    public void setWeatherDescription(String weatherDescription) {
-        this.weatherDescription = weatherDescription;
-    }
-
-    public String getTemp() {
-        return temp;
-    }
-
-    public void setTemp(String temp) {
-        this.temp = temp;
-    }
-
-    public String getTempFeelsLike() {
-        return tempFeelsLike;
-    }
-
-    public void setTempFeelsLike(String tempFeelsLike) {
-        this.tempFeelsLike = tempFeelsLike;
-    }
-
-    public String getPressure() {
-        return pressure;
-    }
-
-    public void setPressure(String pressure) {
-        this.pressure = pressure;
-    }
-
-    public String getHumidity() {
-        return humidity;
-    }
-
-    public void setHumidity(String humidity) {
-        this.humidity = humidity;
-    }
-
-    public String getWindSpeed() {
-        return windSpeed;
-    }
-
-    public void setWindSpeed(String windSpeed) {
-        this.windSpeed = windSpeed;
-    }
-
-    public String getIconUrl() {
-        return iconUrl;
-    }
-
-    public void setIconUrl(String iconUrl) {
-        this.iconUrl = iconUrl;
-    }
-
-    public double getLat() {
-        return lat;
-    }
-
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
-
-    public double getLon() {
-        return lon;
-    }
-
-    public void setLon(double lon) {
         this.lon = lon;
     }
 }
